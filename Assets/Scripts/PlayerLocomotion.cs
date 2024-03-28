29068d496edf39184b33617d664c8fde1680c782
gd-stones <ngducviet2105@gmail.com>
2024-03-28 22:19:11 +0700

    setup input system

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fs : MonoBehaviour
+public class PlayerLocomotion : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
